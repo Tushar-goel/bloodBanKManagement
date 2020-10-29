f7ddd08a5598f5795b9e7ffccf368d5b7b1173dc
Tushar Goel <tushar.goel821@gmail.com>
2020-10-29 22:54:19 +0530

    form control added and navigation added

--- src/app/site/contact/contact.component.ts
+++ src/app/site/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GlobalValidator } from 'src/app/shared/validators/global.validator';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  
+  contactForm: FormGroup;
 
-  constructor() { }
+	constructor(private _fb: FormBuilder) {}
+
 
   ngOnInit() {
+    this.initContactForm();
   }
 
+  // initializing Contact form
+	initContactForm() {
+		this.contactForm = this._fb.group({
+			fullname: [ '', Validators.required ],
+			email: [ '', Validators.compose([ Validators.required, GlobalValidator.emailFormat ]) ],
+			phone: [ '', Validators.compose([ Validators.required, GlobalValidator.phoneFormat ]) ],
+			subject: [ '', Validators.required ],
+			message: [ '', Validators.required ],
+		});
+	}
+
+	submit() {
+		if (!this.contactForm.valid) return true;
+
+		console.log(this.contactForm.value,"**");
+		this.contactForm.reset();
+	}
 }
--- src/app/site/register/register.component.ts
+++ src/app/site/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GlobalValidator } from 'src/app/shared/validators/global.validator';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent implements OnInit {
+	registerForm: FormGroup;
 
-  constructor() { }
+	constructor(private _fb: FormBuilder) {}
 
-  ngOnInit() {
+	ngOnInit() {
+    this.initRegisterForm();
   }
 
+	// initializing register form
+	initRegisterForm() {
+		this.registerForm = this._fb.group({
+			fullname: [ '', Validators.required ],
+			gender: [ '', Validators.compose([ Validators.required ]) ],
+			username: [ '', Validators.compose([ Validators.required, GlobalValidator.emailFormat ]) ],
+			dateofbirth: [ '', Validators.compose([ Validators.required ]) ],
+			address: [ '', Validators.required ],
+			bloodgroup: [ '', Validators.required ],
+			phone: [ '', Validators.compose([ Validators.required, GlobalValidator.phoneFormat ]) ],
+			state: [ '', Validators.required ]
+		});
+	}
+
+	submit() {
+		if (!this.registerForm.valid) return true;
+
+		console.log(this.registerForm.value,"**");
+		this.registerForm.reset();
+	}
 }
